261247cdc6c0888a3208b03af1c9ccac555b6587
Lara <laragabrieleoliv@gmail.com>
2023-04-27 09:36:37 -0300

    mix

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -3,8 +3,7 @@ import {useRouter} from 'next/router';
 import SectionContainer from '@/shared/components/SectionContainer';
 import PageLayout from '@/shared/layout/PageLayout';
 import { db } from '../../../firebase/firebase-config';
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { doc,getDoc} from "firebase/firestore";
 import Banner from '@/shared/components/Banner';
 import Title from '@/shared/components/Title';
 import Column from '@/shared/components/Column';
@@ -14,29 +13,39 @@ import Paragraph from '@/shared/components/Paragraph';
 const Post = () => {
   const [post,setPost] = useState({})
   const router = useRouter();
-  const { id } = router.query;
+  const id:any = router.query.id;
+  const _id:string = typeof id === 'string'?id:'';
+
  
-  useEffect(() => {
-    const docRef = doc(db, "posts", id);
-    getDoc(docRef)
-      .then((doc) => {
-        if (doc.exists()) {
-          const data = doc.data();
-          setPost({
-            title: data.title,
-            content: data.content,
-            date: data.date,
-            image: data.image,
-            author: data.author
-          });
-        } else {
-          console.log("Documento não encontrado!");
-        }
-      })
-      .catch((error) => {
-        console.log("Erro ao obter o documento:", error);
-      });
-  }, [id]);
+   useEffect(()=>{
+     const getPost = async() =>{
+      if(_id){
+        const docRef = doc(db, "posts", id);
+        getDoc(docRef)
+        .then((doc) => {
+          if (doc.exists()) {
+            const data = doc.data();
+            setPost({
+              title: data.title,
+              content: data.content,
+              date: data.date,
+              image: data.image,
+              author: data.author
+            });
+          } else {
+            console.log("Documento não encontrado!");
+            //aqui na verdade vc lida com erro e/ou renderiza um componente de erro/alternativo.
+          }
+        })
+        .catch((error) => {
+          console.log("Erro ao obter o documento:", error);
+            //aqui na verdade vc lida com erro e/ou renderiza um componente de erro/alternativo.
+        });
+      }
+     }
+     getPost()
+  
+   },[_id])
 
  console.log(post)
 
@@ -61,5 +70,28 @@ const Post = () => {
     </PageLayout>
   )
 }
+
+/*
+  if(id){
+      const docRef = doc(db, "posts", id);
+      getDoc(docRef)
+      .then((doc) => {
+        if (doc.exists()) {
+          const data = doc.data();
+          setPost({
+            title: data.title,
+            content: data.content,
+            date: data.date,
+            image: data.image,
+            author: data.author
+          });
+        } else {
+          console.log("Documento não encontrado!");
+        }
+      })
+      .catch((error) => {
+        console.log("Erro ao obter o documento:", error);
+      });
+*/
 
 export default Post;
