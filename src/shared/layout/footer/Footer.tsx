a487a070cd2b09c90f2b6232a164a039e12ae3ec
Kayque <109627285+kayqueprogram@users.noreply.github.com>
2023-04-26 23:47:16 -0300

    Update Footer.tsx

--- src/shared/layout/footer/Footer.tsx
+++ src/shared/layout/footer/Footer.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledFooter = styled.footer`
+  background-color: #f5f5f5;
+  padding: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Footer = () => {
   return (
-    <Container>
-      
-    </Container>
-  )
-}
-
-const Container = styled.div`
- 
-`;
+    <StyledFooter>
+        <p>Coded by: <a href="https://github.com/LaraGb" target='_blank'>Homo</a> and <a href="https://shadowcoder.vercel.app/" target='_blank'>ShadowCoder</a></p> <br/>
+      <p>&copy; 2023 - Todos os direitos reservados.</p>
+    </StyledFooter>
+  );
+};
 
 export default Footer;
